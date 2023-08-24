e5f7c3c49b1eb60c29d2100f10f8236b9f8a46bf
AtulShince27 <atulk.shince@gmail.com>
2023-08-24 23:03:43 +0530

    Update1

--- src/App.js
+++ src/App.js
@@ -1,23 +1,43 @@
 import logo from './logo.svg';
+import { useState, useEffect } from 'react';
+// import Axios from "axios";
 import './App.css';
+import dice from './images/icon-dice.svg';
+import divider from './images/pattern-divider-desktop.svg';
+
+
 
 function App() {
+  const [advice, setAdvice] = useState("");
+  const [adviceId, setAdviceId] = useState(0);
+  useEffect(() => {
+    fetch('https://api.adviceslip.com/advice')
+    .then((res) => res.json())
+    .then((data) => {
+      console.log(data);
+      const slipData = data;
+      console.log(slipData);
+      console.log(slipData.slip.advice);
+      setAdvice(slipData.slip.advice);
+      console.log(advice);
+      setAdviceId(slipData.slip.id);
+      // adviceText.innerText = advice;
+    });
+  });
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="content-container">
+      <div className='ContentCard p-10 ml-auto mr-auto rounded-xl'>
+      <div className="CardHeader text-center mb-10">
+        <p id='adviceId' className='mb-4 font-medium'>ADVICE &nbsp;#{adviceId}</p>
+        <p id='adviceText' className='text-3xl mt-4 font-semibold'>"{advice}"</p>
+        {/* <hr className='mt-11'/> */}
+        <img src={divider} alt="divider" className='mt-10'/>
+      </div>
+    </div>
+      <div className="AdviceGeneratorBtn text-center">
+        {/* <button onClick={() => {setAdviceId();setAdvice();}}>Generate Advice</button> */}
+        <button className='rounded-full p-6' id='diceBtn'><img src={dice} alt="dice" onClick={() => {setAdviceId();setAdvice();}}/></button>
+      </div>
     </div>
   );
 }
